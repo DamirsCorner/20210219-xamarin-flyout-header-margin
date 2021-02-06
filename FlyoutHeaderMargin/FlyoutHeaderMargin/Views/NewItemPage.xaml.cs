501970511cdb583be0d887a970322733fead4a32
Damir Arh <damir.arh@gmail.com>
2021-02-06 10:50:51 +0100

    Add project files.

---
+++ FlyoutHeaderMargin/FlyoutHeaderMargin/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FlyoutHeaderMargin.Models;
+using FlyoutHeaderMargin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlyoutHeaderMargin.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
